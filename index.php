4d40fb93a3db88305f12aed309b7f66c331d7ed0
gabi-p <gabi.p@dealium.ie>
2013-11-04 17:24:43 +0200

    update

--- index.php
+++ index.php
@@ -2,13 +2,14 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <title>kkkk</title>
     </head>
     <body>
         <?php
         // put your code here
         ?>
-        <p>Hey world.</p>
+        
         <h5>Hey world.</h5>
+        Salut
     </body>
 </html>
